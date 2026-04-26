b0a2025412302bde256fdb6bc7d1a74656b3122f
Fady Youssef <62950351+JustFady@users.noreply.github.com>
2026-04-26 15:03:13 -0700

    chore: use lowercase repo name in config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Global-Homicide-Monitor/',
+  base: '/global-homicide-monitor/',
   plugins: [react()],
   root: '.',
 });
