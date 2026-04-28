7f7d322aa89d413c51555ba5251b79ff0043071c
Fady Youssef <62950351+JustFady@users.noreply.github.com>
2026-04-28 15:42:30 -0700

    Correct base path for Organism project deployment

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Global-Homicide-Monitor/',
+  base: '/CPSC481_personalProject/',
   plugins: [react()],
 });
